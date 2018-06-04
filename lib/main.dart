a9bfb7195014567af7b5e28440e0afb2182a9035
Varun Pandey <varunn.pandey@gmail.com>
2018-06-05 01:36:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(home: new MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new CustomScrollView(
+        slivers: <Widget>[
+          new SliverAppBar(
+            leading: new Icon(Icons.tag_faces),
+            expandedHeight: 150.0,
+            elevation: 0.0,
+            pinned: false,
+            floating: true,
+            backgroundColor: Colors.white,
+            flexibleSpace: new FlexibleSpaceBar(
+              background: new Image.network('https://pre00.deviantart.net/6f68/th/pre/f/2012/274/3/a/twitter_minimalistic_background_by_vaughnwhiskey-d5gityo.png',
+              fit: BoxFit.cover ,),
+              title: new Text('Twitter',
+              style: new TextStyle(
+                fontSize: 36.0,
+                color: Colors.white
+              ),
+
+
+            ),
+          ),),
+          new SliverFillRemaining(
+            child: new Container(
+              margin: new EdgeInsets.all(4.0),
+                          child: new ListView.builder(
+                            itemCount: 4,
+                                                      itemBuilder:(context, index){return new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Card(
+                    color: Colors.grey[200],
+                    child: new Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,  
+                    children: <Widget>[
+                    new Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new CircleAvatar(
+                        radius: 25.0,
+                        child: new Icon(Icons.tag_faces),
+                      ),
+                    ),
+                    new Expanded(
+                        child: new Column(
+                        children: <Widget>[
+                            new Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: new Text('India beat Kenya by 3-0. Captain Sunil Chhetri scored 2 goals in his 100th international match'),
+                            ),
+                            new Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: new Image.network('https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/Sunil_chetri_india_jersey_AIFFmedia_750x500_0.jpg?itok=1RdliibZ'),
+                            )
+                        ],
+                      ),
+                    )
+                    ],),
+                  ),
+                ],
+              );}
+                          ),
+            ),
+          )
+
+        ]
+      ),
+      // appBar: new AppBar(
+      //   title: new Text('Twitter UI'),
+      // ),
+      // body: new Padding(
+      //   padding: new EdgeInsets.only(top: 25.0),
+      //         child: new Center(
+      //     child: new Column(
+      //       children: <Widget>[
+      //         new Text('Twitter',
+      //         style: new TextStyle(
+      //           fontSize: 20.0
+      //         ),),
+      //         new Card(
+      //             child: new Row(children: <Widget>[
+      //             new Padding(
+      //               padding: const EdgeInsets.all(8.0),
+      //               child: new CircleAvatar(
+      //                 radius: 25.0,
+      //                 child: new Icon(Icons.supervisor_account),
+      //               ),
+      //             )
+      //           ],),
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: new FloatingActionButton.extended(
+        label: new Text('Trending'),
+        icon: new Icon(Icons.trending_down),
+        onPressed: null,
+      ),
+      bottomNavigationBar: new Container(
+        decoration: new BoxDecoration(
+          boxShadow: [new BoxShadow(
+            blurRadius: 5.0,
+            color: Colors.grey
+          )]
+        ),
+        child: new BottomAppBar(
+          hasNotch: false,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+                  new IconButton(
+                  onPressed: null,
+                  icon: new Icon(Icons.timeline),
+                ),
+                new IconButton(
+                  onPressed: null,
+                  icon: new Icon(Icons.account_circle),
+                )
+            ],
+          ),
+        ),
+        
+      ),
+    );
+  }
+}
